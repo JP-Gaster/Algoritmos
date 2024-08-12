f6438831e1f6439dd89e7df27c62a72a3651a02c
J. P. Gaster <128156817+JP-Gaster@users.noreply.github.com>
2024-08-12 09:02:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const preco_livros = [25, 15, 30, 50, 45, 20];
+const precos_livros = [25, 15, 30, 50, 45, 20];
 
 let atual = 0;
 let mais_barato = 0;
+
+for(let atual = 0; atual < precos_livros.length; atual ++){
+    if(precos_livros[atual] < precos_livros[mais_barato]){
+        mais_barato = atual
+    }
+}
+console.log(`O livro mais barato custa ${precos_livros[mais_barato]}`)
