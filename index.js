d2408dedd3fb86972c548f9ea6562773ec01e567
J. P. Gaster <128156817+JP-Gaster@users.noreply.github.com>
2024-08-12 14:35:04 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const precos_livros = [25, 15, 30, 50, 45, 20];
+const livros = require("./lista_livros")
 
 let atual = 0;
 let mais_barato = 0;
 
-for(let atual = 0; atual < precos_livros.length; atual ++){
-    if(precos_livros[atual] < precos_livros[mais_barato]){
+for(let atual = 0; atual < livros.length; atual ++){
+    if(livros[atual].preco < livros[mais_barato].preco){
         mais_barato = atual
     }
 }
-console.log(`O livro mais barato custa ${precos_livros[mais_barato]}`)
+console.log(`O livro mais barato custa ${livros[mais_barato].preco} e o título é ${livros[mais_barato].preco}`)
---
+++ lista_livros.js
@@ -0,0 +1,28 @@
+const precos_livros = [
+    {
+        titulo: "JavaScript",
+        preco: R$25,
+    },
+    {
+        titulo: "PHP",
+        preco: R$15,
+    },
+    {
+        titulo: "Java",
+        preco: R$30,
+    },
+    {
+        titulo: "Elixir",
+        preco: R$50,
+    },
+    {
+        titulo: "Go",
+        preco: R$45,
+    },
+    {
+        titulo: "Python",
+        preco: R$65,
+    },
+];
+
+module.exports = precos_livros;
